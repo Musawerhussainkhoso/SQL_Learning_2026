dc2e6582f742699584b1e0c54c7d7f775562ef9c
Musawer Hussain <147225635+Musawerhussainkhoso@users.noreply.github.com>
2026-07-10 13:56:31 +0500

    Add Right_Join.sql file for demonstrating the use of RIGHT JOIN in SQL queries

---
+++ Right_Join.sql
@@ -0,0 +1,38 @@
+/*
+SELECT columns
+FROM table1
+JOIN table2
+ON table1.common_column = table2.common_column;
+*/
+CREATE DATABASE stu;
+USE stu;
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50)
+);
+
+CREATE TABLE marks (
+    mark_id INT PRIMARY KEY,
+    student_id INT,
+    subject VARCHAR(50),
+    marks INT
+);
+
+INSERT INTO students VALUES
+(1, 'Ali'),
+(2, 'Sara'),
+(3, 'Ahmed'),
+(4, 'Hina');
+
+INSERT INTO marks VALUES
+(101, 1, 'SQL', 85),
+(102, 2, 'Python', 90),
+(103, 3, 'Excel', 78),
+(104, 5, 'Power BI', 88);
+
+
+SELECT students.student_name, marks.subject, marks.marks
+FROM students
+RIGHT JOIN marks
+ON students.student_id = marks.student_id;
